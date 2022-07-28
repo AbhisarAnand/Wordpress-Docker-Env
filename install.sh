fa18de034cca832d24a22c7a71bf1ee46d739652
abhisaranand <abhisar.muz@gmail.com>
2022-07-28 11:44:51 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt list --upgradable
+
+# Apache2 Installation
+sudo apt -qy install apache2 apache2-doc libexpat1 ssl-cert
+
+# MySQL Installation
+echo "...Installing MySQL..."
+sudo apt install aptitude -y
+sudo apt install mysql-server -y
+sudo systemctl start mysql.service
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Abhisar-123';"
+mysql -u root <<-EOF
+UPDATE mysql.user SET Password=PASSWORD('Abhisar-123') WHERE User='root';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+FLUSH PRIVILEGES;
+EOF
+mysql -u root -p"Abhisar-123" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH auth_socket;"
+mysql -u root -p"Abhisar-123" -e "CREATE USER 'abhisar'@'localhost' IDENTIFIED BY 'Abhisar-123'; GRANT ALL PRIVILEGES ON *.* TO 'abhisar'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+echo "...MySQL Installed..."
+
+# Docker Installation
+echo "...Installing Docker..."
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker abhisar
+echo "...Docker Installed..."
+
+# Docker Compose Installation
+echo "...Installing Docker Compose..."
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+echo "...Docker Compose Installed..."
+
+# phpMyAdmin Installation
+echo "...Installing phpMyAdmin..."
+mysql -u root -p"Abhisar-123" -e "UNINSTALL COMPONENT 'file://component_validate_password';";
+sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
+mysql -u root -p"Abhisar-123" -e "INSTALL COMPONENT 'file://component_validate_password';";
+sudo phpenmod mbstring
+sudo systemctl restart apache2
+echo "...phpMyAdmin Installed..."
